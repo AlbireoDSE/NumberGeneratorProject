7e312959d81fb843de16febec39d31cfec0d2a5b
LiocansG <nico6041@hotmail.be>
2025-02-22 18:19:53 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-from Iterator import EIterator
+from iterator import EIterator
+from generator import EGenerator
 from utils.path_finder import PathFinder
 
 if __name__ == "__main__":
     
     file_path = PathFinder.get_complet_path("files/e2M.txt")
-    iterator = EIterator(file_path)
-    print("".join(next(iterator) for _ in range(10)))
-    print("".join(next(iterator) for _ in range(10)))
+    iterator = EIterator(file_path = file_path)
+    generator = EGenerator(iterator = iterator)
+    
+    print(generator.generate())
