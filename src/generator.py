718e7a1df1e47e93a7e202aaf5505545e4ec7909
LiocansG <nico6041@hotmail.be>
2025-02-22 19:37:12 +0100

    Update generator.py

--- src/generator.py
+++ src/generator.py
@@ -1,13 +1,33 @@
 from iterator import EIterator
 
 class EGenerator:
+    """
+    Generator to generate decimal number.
+    
+    Attributes:
+        iterator (EIterator): Iterator class to iterate over the decimal
+        num_decimals (int): The number of decimal wanted to be used. Defaults = 10.
+    """
 
     def __init__(self, iterator: EIterator, num_decimals: int = 10):
+        """
+        Initializes an EGenerator object.
+
+        Args:
+            iterator (EIterator): Iterator class to iterate over the decimal
+            num_decimals (int): The number of decimal wanted to be used. Defaults = 10.
+        """
         self.iterator = iterator
         self.num_decimals = num_decimals
         self.divider = 10 ** self.num_decimals
     
-    def generate(self):
+    def generate(self) -> float:
+        """
+        Generate a number with X decimals
+
+        Returns:
+            int: generated number
+        """
         decimals = "".join(next(self.iterator) for _ in range(self.num_decimals))
         return int(decimals) / self.divider
 
