d715a14018d01437c093291fceb1cceb3ea92af2
Vancauwenberghe Emilien <la187707@student.helha.be>
2025-02-23 15:09:25 +0100

    Fixing Khi Square

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
-from generator import EGenerator
-from iterator import EIterator
 from src.histogram import Histogram
-from src.khi_square_test import KhiSquareTest
-from utils.path_finder import PathFinder
+from src.tests.khi_square import KhiSquareTest
 
 if __name__ == "__main__":
     
@@ -12,13 +9,13 @@ if __name__ == "__main__":
     
     #print(generator.generate())
 
-    data1 = [0.1, 0.1, 0.5, 0.6, 0.9, 0.99, 0.3, 2]
+    data1 = [0.1, 0.2, 0.01, 0.1, 0.2, 0.01, 0.1, 0.2, 0.01, 0.4, 0.1, 0.5, 0.6, 0.9, 0.99, 0.3, 2, 0.7]
     data2 = [0.1, 0.5, 0.5, 0.6, 0.9, 0.99, 0.3, 2, 0.01, 0.1]
 
     hist_creator = Histogram(interval_nb=5)
 
     hist1 = hist_creator.create(data1)
-    hist2 = hist_creator.create(data1)
+    hist2 = hist_creator.create(data2)
 
 
 
